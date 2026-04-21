c339079c71c3af1fe13ad7c421f7216b82dceb94
douglasbarreiros701 <douglasb701@gmail.com>
2026-04-20 21:42:55 -0300

    template interaction handler /on

--- bot/setups/CreateTemplateCommand_setup.py
+++ bot/setups/CreateTemplateCommand_setup.py
@@ -6,3 +6,10 @@ class CreateTemplateCommand:
     def __init__(self, tree: app_commands.CommandTree):
         self.tree = tree
         self.tree.add_command(self.criar_template)
+
+        @app_commands.command(name='criar_template', description='Crie o template para um evento')
+        async def criar_template(self, interaction: discord.Interaction):
+            await interaction.response.send_message('Digite o nome do evento', ephemeral=True)
+            #interaction!!!!1
+
+            handler =
